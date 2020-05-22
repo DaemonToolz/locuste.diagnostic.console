4dc0436ffbda442d8ca3399e59b6ed3fcc3fd564
Axel MACIEJEWSKI <axel.maciejewski@viacesi.fr>
2020-05-22 15:10:27 +0200

    chore(naming): preparing names for diagnostic pipe

--- locuste.system.connector.cpp
+++ locuste.system.connector.cpp
@@ -2,6 +2,13 @@
 #include "./pipes/PipeHandler.cpp"
 using namespace std;
 
+
+/*
+"locuste.brain.diagnostic" => Write
+"locuste.diagnostic.brain" => Read
+"locuste.scheduler.diagnostic" => Write
+"locuste.diagnostic.scheduler" => Read
+*/
 int main (int argc, const char * argv[]) {
     return 0;
 }
